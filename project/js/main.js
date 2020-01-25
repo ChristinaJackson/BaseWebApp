c1c751c2d0f8df3d3a4ca1626f20738cb62c7a7a
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2020-01-25 15:48:55 -0800

    weather api connected and showing

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,21 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+// function showPicture(){
+//   // use jQuery ($ is shorthand) to find the div on the page and then change the html
+//   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+//   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
+//   $("p").html("High five! You're building your first web app!");
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+//   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
+// }
+
+$(document).ready(function(){
+  getWeather();
+})
+
+function getWeather(){
+  let url = 'https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&APPID='+apiKey;
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }})
 }
