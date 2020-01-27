b7bbe6fa0bbc306d86c59132ef40fcc387e97a2c
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2020-01-27 15:41:26 -0800

    Auth added and publishing post to the page

--- project/js/main.js
+++ project/js/main.js
@@ -12,6 +12,10 @@
 //   getWeather();
 // })
 
+$(document).ready(function(){
+  getPosts();
+})
+
 function getWeather(searchQuery){
   let url = 'https://api.openweathermap.org/data/2.5/weather?q='+searchQuery+'&units=imperial&APPID='+apiKey;
   $.ajax(url,{success: function(data){
@@ -28,4 +32,60 @@ function getWeather(searchQuery){
 function searchWeather(){
   let searchQuery = $(".search").val();
   getWeather(searchQuery);
+}
+
+
+//This is for the message app
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email)
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+}
+function addMessage(postTitle, postBody){
+  let postData = {
+    title: postTitle,
+    body: postBody
+  }
+  var database = firebase.database().ref('posts');
+  var newPostRef = database.push();
+  newPostRef.set(postData, function(error) {
+    if (error) {
+      console.log('it did not work')
+    } else {
+      window.location.reload(true);
+    }
+  });
+}
+
+function handleMessageFormSubmit(){
+  let postTitle = $("#post-title").val();
+  let postBody = $("#post-body").val();
+  console.log(postTitle)
+  addMessage(postTitle,postBody);
+}
+
+function getPosts(){
+  return firebase.database().ref('posts').once('value').then(function(snapshot) {
+    var posts = snapshot.val();
+    for(var postKey in posts){
+      var post = posts[postKey];
+      $("#post-listing").append("<div>"+ post.title + " - "+ post.body + "</div>");
+    }
+  });
+  
 }
